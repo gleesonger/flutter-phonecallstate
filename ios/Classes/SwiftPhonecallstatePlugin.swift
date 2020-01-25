35e46c60090a4ac37b027ad44bfb4bacc42c216a
Gerard Gleeson <gleesonger@hotmail.com>
2020-01-25 14:51:32 +0000

    Pre iOS dummy funtionality

--- ios/Classes/SwiftPhonecallstatePlugin.swift
+++ ios/Classes/SwiftPhonecallstatePlugin.swift
@@ -1,88 +1,102 @@
-import Flutter
-import UIKit
-import CallKit
+#if canImport(UIKit) && canImport(CallKit)
+
+	import Flutter
+	import UIKit
+	import CallKit
+
+	public class SwiftPhonecallstatePlugin: NSObject, FlutterPlugin, CXCallObserverDelegate {
+
+		var callObserver: CXCallObserver!
+		var _channel: FlutterMethodChannel
+
+		var testTimer: Timer?
+		var counter = 0
 
 
-@available(iOS 10.0,*)
-public class SwiftPhonecallstatePlugin: NSObject, FlutterPlugin, CXCallObserverDelegate {
+		public static func register(with registrar: FlutterPluginRegistrar) {
+			let channel = FlutterMethodChannel(name: "com.plusdt.phonecallstate", binaryMessenger: registrar.messenger())
+			let instance = SwiftPhonecallstatePlugin(channel:channel)
+			registrar.addMethodCallDelegate(instance, channel: channel)
+		}
 
-    var callObserver: CXCallObserver!
-    var _channel: FlutterMethodChannel
+		init(channel:FlutterMethodChannel){
+			super.init()
+			_channel = channel
 
-    var testTimer: Timer?
-    var counter = 0
-
-
-    public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "com.plusdt.phonecallstate", binaryMessenger: registrar.messenger())
-    let instance = SwiftPhonecallstatePlugin(channel:channel)
-    registrar.addMethodCallDelegate(instance, channel: channel)
+			setupCallObserver()
+		}
 
 
-    }
+		@objc func timerAction() {
+			counter += 1
+			_channel.invokeMethod("phone.incoming", arguments: nil)
+			print("Timer!")
+			//testTimer.invalidate()
+		}
 
-    init(channel:FlutterMethodChannel){
-        super.init()
-        _channel = channel
+		public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+			print("iOS => call \(call.method)")
 
-        setupCallObserver()
-    }
+			switch (call.method) {
+				case "phoneTest.PhoneIncoming":
+					var testTimer = Timer.scheduledTimer(timeInterval: call.arguments as! Double,
+									target: self,
+									selector: #selector(SwiftPhonecallstatePlugin.timerAction),
+									userInfo: nil,
+									repeats: true)
+			default:
+				result(FlutterMethodNotImplemented)
+			}
 
+			result(1)
+		}
 
-    @objc func timerAction() {
-        counter += 1
-        _channel.invokeMethod("phone.incoming", arguments: nil)
-        print("Timer!")
-        //testTimer.invalidate()
-    }
+		func setupCallObserver(){
+				callObserver = CXCallObserver()
+				callObserver.setDelegate(self, queue: nil)
+		}
 
-    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-        print("iOS => call \(call.method)")
+		public func callObserver(_ callObserver: CXCallObserver, callChanged call: CXCall) {
+			if call.hasEnded == true {
+				print("CXCallState :Disconnected")
+				 _channel.invokeMethod("phone.disconnected", arguments: nil)
+			}
+			if call.isOutgoing == true && call.hasConnected == false {
+				print("CXCallState :Dialing")
+				_channel.invokeMethod("phone.dialing", arguments: nil)
+			}
+			if call.isOutgoing == false && call.hasConnected == false && call.hasEnded == false {
+				print("CXCallState :Incoming")
+				_channel.invokeMethod("phone.incoming", arguments: nil)
+			}
 
-        switch (call.method) {
-            case "phoneTest.PhoneIncoming":
-                var testTimer = Timer.scheduledTimer(timeInterval: call.arguments as! Double,
-                                target: self,
-                                selector: #selector(SwiftPhonecallstatePlugin.timerAction),
-                                userInfo: nil,
-                                repeats: true)
-        default:
-            result(FlutterMethodNotImplemented)
-        }
+			if call.hasConnected == true && call.hasEnded == false {
+				print("CXCallState : Connected")
+				 _channel.invokeMethod("phone.connected", arguments: nil)
+			}
+		}
 
-        result(1)
-    }
+	}
 
-    @available(iOS 10.0,*)
-    func setupCallObserver(){
-            callObserver = CXCallObserver()
-            callObserver.setDelegate(self, queue: nil)
-    }
+#else
 
+	import Flutter
+	import UIKit
 
-    @available(iOS 10.0,*)
-    public func callObserver(_ callObserver: CXCallObserver, callChanged call: CXCall) {
-        if call.hasEnded == true {
-            print("CXCallState :Disconnected")
-             _channel.invokeMethod("phone.disconnected", arguments: nil)
-        }
-        if call.isOutgoing == true && call.hasConnected == false {
-            print("CXCallState :Dialing")
-            _channel.invokeMethod("phone.dialing", arguments: nil)
-        }
-        if call.isOutgoing == false && call.hasConnected == false && call.hasEnded == false {
-            print("CXCallState :Incoming")
-            _channel.invokeMethod("phone.incoming", arguments: nil)
-        }
+	public class SwiftFlutterAlertDemoPlugin: NSObject, FlutterPlugin {
 
-        if call.hasConnected == true && call.hasEnded == false {
-            print("CXCallState : Connected")
-             _channel.invokeMethod("phone.connected", arguments: nil)
-        }
-    }
+		public static func register(with registrar: FlutterPluginRegistrar) {
+			let channel = FlutterMethodChannel(name: "flutter_alert_demo", binaryMessenger: registrar.messenger())
+			let instance = SwiftFlutterAlertDemoPlugin()
+			registrar.addMethodCallDelegate(instance, channel: channel)
+		}
+		
+		public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+			result(0)
+		}
+	}
 
-}
-
+#endif
 
 // reference
 // https://stackoverflow.com/questions/42557701/can-callkit-be-used-with-non-voip-call-to-get-the-call-states-in-ios?rq=1
